14af59afd7b1668d142dc5b91c2cdf3ee4c23344
anechol <ashleyne92@gmail.com>
2016-07-12 13:23:29 -0500

    Adds start page but needs to fix draw function.

--- app/js/main.js
+++ app/js/main.js
@@ -9,13 +9,13 @@ var randomNum = function(min, max) {
 
 var playerScore = 0;
 var opponentScore = 0;
-var gameEnd = false;
+var playing;
 
 var animate = (function() {
 	return window.requestAnimationFrame || function(callback) {
-		return window.setTimeout(callback, 1000/60);
+		return window.setTimeout(callback, 1000 / 60);
 	};
-}) ();
+})();
 
 function init() {
 	animate(draw);
@@ -23,7 +23,7 @@ function init() {
 
 function clear() {
 	// Field - This is the entire field, colored black.
-	c.clearRect(0,0,canvas.width,canvas.height);
+	c.clearRect(0, 0, canvas.width, canvas.height);
 	canvas.style.backgroundColor = "black";
 	c.save();
 
@@ -31,7 +31,7 @@ function clear() {
 	c.strokeStyle = "white";
 	c.setLineDash([10, 5]);
 	c.beginPath();
-	c.moveTo(349,0);
+	c.moveTo(349, 0);
 	c.lineTo(349, 401);
 	c.stroke();
 	c.save();
@@ -47,7 +47,7 @@ var player = {
 	color: "white",
 	draw: function() {
 		c.fillStyle = this.color;
-		c.fillRect(this.x,this.y,this.width,this.height);
+		c.fillRect(this.x, this.y, this.width, this.height);
 	}
 };
 
@@ -65,14 +65,14 @@ var opponent = {
 	color: "white",
 	draw: function() {
 		c.fillStyle = this.color;
-		c.fillRect(this.x,this.y,this.width,this.height);
+		c.fillRect(this.x, this.y, this.width, this.height);
 	}
 };
 
 function opponentMove() {
 	// Opponent tracks balls and hits in random place, simulating AI
 	// FIXME: Must put a margin of error here so the AI doesn't cheat
-	opponent.y = ball.y - randomNum(30,30);
+	opponent.y = ball.y - randomNum(30, 30);
 }
 
 // Ball Object
@@ -86,12 +86,11 @@ var ball = {
 	color: "white",
 	draw: function() {
 		c.fillStyle = this.color;
-		c.fillRect(this.x,this.y,this.width,this.height);
+		c.fillRect(this.x, this.y, this.width, this.height);
 	}
 };
 
 function score() {
-	// FIXME: Bruh, you gotta fix this. Can't do much else without it.
 	if (ball.x > canvas.width) {
 		playerScore++;
 	}
@@ -107,6 +106,8 @@ function score() {
 	c.fillText(opponentScore, 520, 30);
 }
 
+
+
 function reset() {
 	// Ball reappears in center if crossing the "goals"
 	if (ball.x > canvas.width || ball.x < 0) {
@@ -115,30 +116,54 @@ function reset() {
 		ball.vx = 5;
 		ball.vy = 5;
 	}
-	if(playerScore == 2 || opponentScore == 2) {
+	if (playerScore == 2 || opponentScore == 2) {
 		ball.x = canvas.width / 2;
 		ball.y = canvas.height / 2;
+		clear();
+		gameEnd = true;
 		c.font = "36px sans-serif";
 		c.fillStyle = "white";
 		c.fillText("Game Over!", 250, 200);
-		gameEnd = true;
 	}
-	// if (gameEnd = true) {
-	//
-	// }
+	if (gameEnd = true) {
+		canvas.onclick = function() {
+			startGame();
+		}
+	}
 }
 // TODO: Add in a start and end screen where you have to click a button to continue
 
+var startButton = function() {
+	c.font = "36px sans-serif";
+	c.fillStyle = "white";
+	c.fillText("Start", 310, 210);
+}
+
 // Draws everything on the Canvas
 function draw() {
-	clear();
-	player.draw();
-	opponent.draw();
-	opponentMove();
-	score();
-	reset();
+	function startGame() {
+		window.onload = function() {
+			clear();
+			startButton();
+			// playing = false;
+		}
+		canvas.addEventListener('click', function() {
+			startButton();
+		});
+		canvas.onclick = function() {
+			canvas.removeEventListener('click', startButton);
 
-	ball.draw();
+			animate(draw);
+			clear();
+			player.draw();
+			opponent.draw();
+			opponentMove();
+			score();
+			reset();
+			ball.draw();
+		}
+	}
+
 	// Ball Velocity
 	ball.x += ball.vx;
 	ball.y += ball.vy;
@@ -148,14 +173,14 @@ function draw() {
 		ball.vy = -ball.vy;
 	}
 	if (ball.x > canvas.width || ball.x < 0) {
-		ball. vx = -ball.vx;
+		ball.vx = -ball.vx;
 	}
 
 	// Player Collision with Ball
 	if ((ball.x + ball.vx) < (player.x + player.width) && (ball.x + ball.width) > player.x && (ball.y + ball.vy) < (player.y + player.height) && (ball.y + ball.height) > player.y) {
 		// Makes the ball's speed vary when hitting player brick
-		ball.vx = -ball.vx - randomNum(2,4);
-		ball.vy = -ball.vy - randomNum(2,4);
+		ball.vx = -ball.vx - randomNum(2, 4);
+		ball.vy = -ball.vy - randomNum(2, 4);
 		if (ball.x + ball.vx > 7) {
 			ball.x + ball.vx == 5;
 		}
@@ -163,13 +188,14 @@ function draw() {
 	// Opponent Collision with Ball
 	if ((ball.x + ball.vx) < (opponent.x + opponent.width) && (ball.x + ball.width) > opponent.x && (ball.y + ball.vy) < (opponent.y + opponent.height) && (ball.y + ball.height) > opponent.y) {
 		// Makes the ball's speed vary when hitting opponent brick
-		ball.vx = -ball.vx - randomNum(2,4);
-		ball.vy = -ball.vy - randomNum(2,4);
+		ball.vx = -ball.vx - randomNum(2, 4);
+		ball.vy = -ball.vy - randomNum(2, 4);
 		if (ball.x + ball.vx > 7) {
 			ball.x + ball.vx == 5;
 		}
 	}
 
+	startGame();
 	animate(draw);
 }
 
