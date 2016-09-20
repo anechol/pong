74d8af4258506c86a884a6a6d05d4a5995da771a
anechol <ashleyne92@gmail.com>
2016-09-20 15:08:00 -0500

    Fixes AI to allow player to score.

--- app/js/main.js
+++ app/js/main.js
@@ -3,215 +3,219 @@ var c = canvas.getContext('2d');
 var startPage = document.getElementById('startPage');
 var gameOver = document.getElementById('gameOver');
 
-var randomNum = function(min, max) {
-	return Math.random() * (max - min) + min;
+var randomNum = function (min, max) {
+  return Math.random() * (max - min) + min;
 };
 
 var playerScore = 0;
 var opponentScore = 0;
 
-var animate = (function() {
-	return window.requestAnimationFrame || function(callback) {
-		return window.setTimeout(callback, 1000 / 60);
-	};
+var animate = (function () {
+  return window.requestAnimationFrame || function (callback) {
+    return window.setTimeout(callback, 1000 / 60);
+  };
 })();
 
 function init() {
-	animate(draw);
+  animate(draw);
 }
 
 function clear() {
-	// Field - This is the entire field, colored black.
-	c.clearRect(0, 0, canvas.width, canvas.height);
-	canvas.style.backgroundColor = "black";
-	c.save();
+  // Field - This is the entire field, colored black.
+  c.clearRect(0, 0, canvas.width, canvas.height);
+  canvas.style.backgroundColor = 'black';
+  c.save();
 
-	// Center Division - Divides player sides. Cosmetics only.
-	c.strokeStyle = "white";
-	c.setLineDash([10, 5]);
-	c.beginPath();
-	c.moveTo(349, 0);
-	c.lineTo(349, 401);
-	c.stroke();
-	c.save();
+  // Center Division - Divides player sides. Cosmetics only.
+  c.strokeStyle = 'white';
+  c.setLineDash([10, 5]);
+  c.beginPath();
+  c.moveTo(349, 0);
+  c.lineTo(349, 401);
+  c.stroke();
+  c.save();
 }
-
 
 // Player Brick
 var player = {
-	x: 40,
-	y: mouse,
-	width: 20,
-	height: 60,
-	color: "white",
-	draw: function() {
-		c.fillStyle = this.color;
-		c.fillRect(this.x, this.y, this.width, this.height);
-	}
+  x: 40,
+  y: mouse,
+  width: 20,
+  height: 60,
+  color: 'white',
+  draw: function () {
+    c.fillStyle = this.color;
+    c.fillRect(this.x, this.y, this.width, this.height);
+  }
 };
 
 // Maps player brick to mouse's Y position for movement
-var mouse = canvas.addEventListener('mousemove', function(e) {
-	player.y = e.offsetY;
+var mouse = canvas.addEventListener('mousemove', function (e) {
+  player.y = e.offsetY;
 });
 
 // Opponent Brick
 var opponent = {
-	x: 640,
-	y: 120,
-	width: 20,
-	height: 60,
-	color: "white",
-	draw: function() {
-		c.fillStyle = this.color;
-		c.fillRect(this.x, this.y, this.width, this.height);
-	}
+  x: 640,
+  y: 120,
+  width: 20,
+  height: 60,
+  color: 'white',
+  draw: function () {
+    c.fillStyle = this.color;
+    c.fillRect(this.x, this.y, this.width, this.height);
+  }
 };
 
 function opponentMove() {
-	// Opponent tracks balls and hits in random place, simulating AI
-	// FIXME: Must put a margin of error here so the AI doesn't cheat
-	var diff = -((opponent.y + (opponent.height / 2)) - ball.y);
-	if (diff < 0 && diff < -4) {
-		diff = -5;
-	}
-	else if (diff > 0 && diff > 4) {
-		diff = 5;
-	}
+  // Opponent tracks balls and hits in random place, simulating AI
+  // FIXME: Must put a margin of error here so the AI doesn't cheat
+  var diff = -((opponent.y + (opponent.height / 2)) - ball.y);
+  if (diff < 0 && diff < -4) {
+    diff = -5;
+  } else if (diff > 0 && diff > 4) {
+    diff = 5;
+  }
 
-	opponent.y = ball.y - randomNum(diff, diff) - 10;
+  opponent.y += diff;
 }
 
 // Ball Object
 var ball = {
-	x: canvas.width / 2,
-	y: canvas.height / 2,
-	vx: 5,
-	vy: 5,
-	width: 8,
-	height: 8,
-	color: "white",
-	draw: function() {
-		c.fillStyle = this.color;
-		c.fillRect(this.x, this.y, this.width, this.height);
-	}
+  x: canvas.width / 2,
+  y: canvas.height / 2,
+  vx: 5,
+  vy: 5,
+  width: 8,
+  height: 8,
+  color: 'white',
+  draw: function () {
+    c.fillStyle = this.color;
+    c.fillRect(this.x, this.y, this.width, this.height);
+  }
 };
 
 function score() {
-	// Players score when the ball reaches the edges of the screens
-	if (ball.x > canvas.width) {
-		playerScore++;
-	}
-	if (ball.x < 0) {
-		opponentScore++;
-	}
-	c.font = "36px sans-serif";
-	c.fillStyle = "white";
-	c.fillText(playerScore, 165, 30);
+  // Players score when the ball reaches the edges of the screens
+  if (ball.x > canvas.width) {
+    playerScore++;
+  }
 
-	c.font = "36px sans-serif";
-	c.fillStyle = "white";
-	c.fillText(opponentScore, 520, 30);
+  if (ball.x < 0) {
+    opponentScore++;
+  }
+
+  c.font = '36px sans-serif';
+  c.fillStyle = 'white';
+  c.fillText(playerScore, 165, 30);
+
+  c.font = '36px sans-serif';
+  c.fillStyle = 'white';
+  c.fillText(opponentScore, 520, 30);
 }
 
 function startGame() {
-	window.onload = function() {
-		startPage.style.display = 'block';
-		canvas.style.display = 'none';
-		gameOver.style.display = 'none';
-	};
+  window.onload = function () {
+    startPage.style.display = 'block';
+    canvas.style.display = 'none';
+    gameOver.style.display = 'none';
+  };
 
-	startPage.onclick = function() {
-		startPage.style.display = 'none';
-		canvas.style.display = 'block';
-		gameOver.style.display = 'none';
-	};
-	if (canvas.style.display == 'block') {
-		// FIXME: Put a stop animation here because the game seems to be
-		// running when the screen isn't up.
-		clear();
-		player.draw();
-		opponent.draw();
-		opponentMove();
-		score();
-		ball.draw();
-	}
+  startPage.onclick = function () {
+    startPage.style.display = 'none';
+    canvas.style.display = 'block';
+    gameOver.style.display = 'none';
+  };
+
+  if (canvas.style.display == 'block') {
+    // FIXME: Maybe put a stop animation here because the game seems to be
+    // running when the screen isn't up.
+    clear();
+    player.draw();
+    opponent.draw();
+    opponentMove();
+    score();
+    ball.draw();
+  }
 }
 
 function reset() {
-	// Ball reappears in center if crossing the "goals"
-	if (ball.x > canvas.width || ball.x < 0) {
-		// Make the ball reappear in the center with velocity of
-		ball.x = canvas.width / 2;
-		ball.y = canvas.height / 2;
-		ball.vx = 5;
-		ball.vy = 5;
-	}
-	// When either player reaches the score goal, the game ends, and the Game Over screen shows
-	if (playerScore == 5 || opponentScore == 5) {
-		ball.x = canvas.width / 2;
-		ball.y = canvas.height / 2;
+  // Ball reappears in center if crossing the "goals"
+  if (ball.x > canvas.width || ball.x < 0) {
+    // Make the ball reappear in the center with velocity of
+    ball.x = canvas.width / 2;
+    ball.y = canvas.height / 2;
+    ball.vx = 5;
+    ball.vy = 5;
+  }
 
-		startPage.style.display = 'none';
-		canvas.style.display = 'none';
-		gameOver.style.display = 'block';
+  // When either player reaches the score goal, the game ends, and the Game Over screen shows
+  if (playerScore == 5 || opponentScore == 5) {
+    ball.x = canvas.width / 2;
+    ball.y = canvas.height / 2;
 
-		// This lets the game loop when it's reset after a win/loss.
-		if (gameOver.style.display == 'block') {
-			gameOver.onclick = function() {
-				startPage.style.display = 'block';
-				canvas.style.display = 'none';
-				gameOver.style.display = 'none';
+    startPage.style.display = 'none';
+    canvas.style.display = 'none';
+    gameOver.style.display = 'block';
 
-				playerScore = 0;
-				opponentScore = 0;
+    // This lets the game loop when it's reset after a win/loss.
+    if (gameOver.style.display == 'block') {
+      gameOver.onclick = function () {
+        startPage.style.display = 'block';
+        canvas.style.display = 'none';
+        gameOver.style.display = 'none';
 
-				ball.x = canvas.width / 2;
-				ball.y = canvas.height / 2;
+        playerScore = 0;
+        opponentScore = 0;
 
-				ball.vx = 5;
-				ball.vy = 5;
-			};
-		}
-	}
+        ball.x = canvas.width / 2;
+        ball.y = canvas.height / 2;
+
+        ball.vx = 5;
+        ball.vy = 5;
+      };
+    }
+  }
 }
 
 // Draws everything on the Canvas
 function draw() {
-	startGame();
-	reset();
+  startGame();
+  reset();
 
-	// Ball Velocity
-	ball.x += ball.vx;
-	ball.y += ball.vy;
+  // Ball Velocity
+  ball.x += ball.vx;
+  ball.y += ball.vy;
 
-	// Ball Collision with Walls
-	if (ball.y > canvas.height || ball.y < 0) {
-		ball.vy = -ball.vy;
-	}
-	if (ball.x > canvas.width || ball.x < 0) {
-		ball.vx = -ball.vx;
-	}
+  // Ball Collision with Walls
+  if (ball.y > canvas.height || ball.y < 0) {
+    ball.vy = -ball.vy;
+  }
 
-	// Player Collision with Ball
-	if ((ball.x + ball.vx) < (player.x + player.width) && (ball.x + ball.width) > player.x && (ball.y + ball.vy) < (player.y + player.height) && (ball.y + ball.height) > player.y) {
-		// Makes the ball's speed vary when hitting player brick
-		ball.vx = -ball.vx - randomNum(2, 4);
-		ball.vy = -ball.vy - randomNum(2, 4);
-		if (ball.x + ball.vx > 7) {
-			ball.x + ball.vx == 5;
-		}
-	}
-	// Opponent Collision with Ball
-	if ((ball.x + ball.vx) < (opponent.x + opponent.width) && (ball.x + ball.width) > opponent.x && (ball.y + ball.vy) < (opponent.y + opponent.height) && (ball.y + ball.height) > opponent.y) {
-		// Makes the ball's speed vary when hitting opponent brick
-		ball.vx = -ball.vx - randomNum(2, 4);
-		ball.vy = -ball.vy - randomNum(2, 4);
-		if (ball.x + ball.vx > 7) {
-			ball.x + ball.vx == 5;
-		}
-	}
-	animate(draw);
+  if (ball.x > canvas.width || ball.x < 0) {
+    ball.vx = -ball.vx;
+  }
+
+  // Player Collision with Ball
+  if ((ball.x + ball.vx) < (player.x + player.width) && (ball.x + ball.width) > player.x && (ball.y + ball.vy) < (player.y + player.height) && (ball.y + ball.height) > player.y) {
+    // Makes the ball's speed vary when hitting player brick
+    ball.vx = -ball.vx - randomNum(2, 4);
+    ball.vy = -ball.vy - randomNum(2, 4);
+    if (ball.x + ball.vx > 7) {
+      ball.x + ball.vx == 5;
+    }
+  }
+  // Opponent Collision with Ball
+  if ((ball.x + ball.vx) < (opponent.x + opponent.width) && (ball.x + ball.width) > opponent.x && (ball.y + ball.vy) < (opponent.y + opponent.height) && (ball.y + ball.height) > opponent.y) {
+    // Makes the ball's speed vary when hitting opponent brick
+    ball.vx = -ball.vx - randomNum(2, 4);
+    ball.vy = -ball.vy - randomNum(2, 4);
+    if (ball.x + ball.vx > 7) {
+      ball.x + ball.vx == 5;
+    }
+  }
+
+  animate(draw);
 }
 
 init();
